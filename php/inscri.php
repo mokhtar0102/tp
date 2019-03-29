f9471384d74db95cc7ff917cd57d339e06ee0e66
mokhtar0102 <mokhtarajili1@gmail.com>
2019-03-29 21:16:08 +0100

    affichage les donnee avec php

---
+++ php/inscri.php
@@ -0,0 +1,24 @@
+<?php 
+
+$nom=$_GET['nom'];
+$prenom=$_GET['prenom'];
+$tel=$_GET['tel'];
+$sex=$_GET['sex'];
+
+$date=$_GET['date'];
+$nationalite=$_GET['nationalite'];
+
+$email=$_GET['email'];
+
+$mdp=$_GET['mdp'];
+
+echo $nom.'<br/>'; 
+echo $prenom.'<br/>';
+echo $tel.'<br/>' ;
+echo $sex.'<br/>' ;
+echo $date.'<br/>'; ; 
+echo $nationalite.'<br/>'; ;
+ echo $email.'<br/>';;
+echo $mdp.'<br/>';;
+
+?>
---
+++ php/test.php
@@ -0,0 +1,5 @@
+<?php
+
+$nom=$_GET['t1'];
+echo 'bonjour'.' '.$nom;
+?>
